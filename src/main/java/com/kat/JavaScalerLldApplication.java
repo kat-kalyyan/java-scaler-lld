dd15e3f829fe85b17a044a1f54a3b141d1bd5e29
kat-kalyyan <32221979+kat-kalyyan@users.noreply.github.com>
2022-05-15 13:59:40 +0530

    initial commit

---
+++ src/main/java/com/kat/JavaScalerLldApplication.java
@@ -0,0 +1,13 @@
+package com.kat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScalerLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScalerLldApplication.class, args);
+    }
+
+}

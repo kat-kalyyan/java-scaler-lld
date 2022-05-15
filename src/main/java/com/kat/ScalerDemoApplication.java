a7f644c19c8d0425a60eb3db5eeb95ce49d0daa8
kat-kalyyan <32221979+kat-kalyyan@users.noreply.github.com>
2022-05-15 14:04:03 +0530

    refactor

--- src/main/java/com/kat/ScalerDemoApplication.java
+++ src/main/java/com/kat/ScalerDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaScalerLldApplication {
+public class ScalerDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaScalerLldApplication.class, args);
+        SpringApplication.run(ScalerDemoApplication.class, args);
     }
 
 }
